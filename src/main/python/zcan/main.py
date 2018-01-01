eba738db1901b33627a0b69c8e1e1eb60ebe2c04
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2018-01-01 16:55:38 +0100

    separate main

--- src/main/python/zcan/main.py
+++ src/main/python/zcan/main.py
@@ -1,29 +1,24 @@
 from multiprocessing import Process, Manager
-
+from zcan.can import CanBusReader
 import time
 
-from zcan.can import CanBusReader
 
-manager = Manager()
-d = manager.dict()
+def main():
+    manager = Manager()
+    d = manager.dict()
 
+    def input(data):
+        CanBusReader().read_messages(data)
 
-def input(data):
-    reader = CanBusReader()
-    reader.
-    CanBusReader().read_messages(data)
+    def output(data):
+        while True:
+            print(data)
+            time.sleep(2)
 
+    p1 = Process(target=input, args=(d,))
+    p2 = Process(target=output, args=(d,))
 
-def output(data):
-    while True:
-        print(data)
-        time.sleep(2)
-
-
-p1 = Process(target=input, args=(d,))
-p2 = Process(target=output, args=(d,))
-
-p1.start()
-p2.start()
-p1.join()
-p2.join()
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
